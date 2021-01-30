8b3c27e854e716e2f1dbd22eeb5392cf3398c9da
Muhammad Usman <61974138+usmandevops123@users.noreply.github.com>
2021-01-30 15:16:19 +0500

    upload

---
+++ Array/app.js
@@ -0,0 +1,14 @@
+const company=[
+    {name='company1',category='finance',start='2000',end='2005' },
+    {name='company2',category='Retail',start='2000',end='2005' },
+    {name='company3',category='finance',start='2000',end='2005' },
+    {name='company4',category='finance',start='2000',end='2005' },
+    {name='company5',category='finance',start='2000',end='2005' },
+    {name='company6',category='finance',start='2000',end='2005' },
+    
+
+
+
+
+
+]
