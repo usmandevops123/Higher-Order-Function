c15a2fe28b94d5609a9ec6c0ba6861de2aea9046
Muhammad Usman <61974138+usmandevops123@users.noreply.github.com>
2021-01-30 15:43:32 +0500

    Update app.js

--- Array/app.js
+++ Array/app.js
@@ -1,14 +1,20 @@
-const company=[
-    {name='company1',category='finance',start='2000',end='2005' },
-    {name='company2',category='Retail',start='2000',end='2005' },
-    {name='company3',category='finance',start='2000',end='2005' },
-    {name='company4',category='finance',start='2000',end='2005' },
-    {name='company5',category='finance',start='2000',end='2005' },
-    {name='company6',category='finance',start='2000',end='2005' },
+ {name:'company1',category:'finance',start:2000,end:2005 },
+    {name:'company2',category:'Retail',start:2001,end:2006 },
+    {name:'company3',category:'health',start:2002,end:2007 },
+    {name:'company4',category:'sport',start:2003,end:2008 },
+    {name:'company5',category:'pharmacy',start:2004,end:2009 },
+    {name:'company6',category:'education',start:2005,end:2015 }
     
 
+];
+const ages=[33,13,34,21,45,52,12,34,13,24,21,42,12,13,32,13,21];  
 
-
-
-
-]
+for(let i=0;i<companies.length;i++)
+{
+    console.log(companies[i]);
+}
+//forEach
+//filter
+//map
+//sort
+//reduce
